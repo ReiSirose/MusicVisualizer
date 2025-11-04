13d2bc2d90bdd1b1a4b8ec35fce8e6da3ecc6ef6
ReiSirose <captainba123@gmail.com>
2025-11-04 10:12:47 -0600

    fix linker issues for geometry class

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "window.h"
 #include "audio.h"
-
+#include "geometry.h"
 int main (){
     Audio audio ("flower_thief.mp3", 44100, 1024);
     Window window ("flower thief", 1280, 720);
+    float vertices [] = {1.0f, 0.3f, 0.7f};
+    Geometry barGeometry (vertices, 3);
 
     while(!window.shouldClose()){
         window.startFrame();
